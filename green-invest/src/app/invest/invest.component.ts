8347e73c1204e627b55a23b6a5aeb6ac531767c3
alishapatel7 <114514878+alishapatel7@users.noreply.github.com>
2024-06-26 17:32:58 -0400

    Update invest.component.ts

--- green-invest/src/app/invest/invest.component.ts
+++ green-invest/src/app/invest/invest.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-invest',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule, FormsModule],
     templateUrl: './invest.component.html',
     styleUrl: './invest.component.css'
 })
 export class InvestComponent {
     title = 'green-invest';
+    public cost = 45.8
+    public numshares = 0
+    public total = 0; 
+
+    calctotal() : number {
+        this.total = (this.numshares * this.cost);
+        return parseFloat(this.total.toFixed(5));
+    }
+    
 }
