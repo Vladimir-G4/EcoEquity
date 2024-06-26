556bcc1674fe911255f00b0b917411ee57615535
Grace Wei <gracetw210@gmail.com>
2024-06-26 18:30:09 -0400

    Push learning screen - issues with tab text

--- green-invest/src/app/learn/learn.component.ts
+++ green-invest/src/app/learn/learn.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
 
 @Component({
-    selector: 'app-learn',
-    standalone: true,
-    imports: [RouterOutlet],
-    templateUrl: './learn.component.html',
-    styleUrl: './learn.component.css'
+  selector: 'app-learn',
+  standalone: true,
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './learn.component.html',
+  styleUrl: './learn.component.css'
 })
 export class LearnComponent {
-    title = 'green-invest';
+  title = 'green-invest';
+  activeTab: string = 'Tab1';
+  openTab(tabName: string): void {
+    this.activeTab = tabName;
+    console.log(this.activeTab);
+  }
 }
+
