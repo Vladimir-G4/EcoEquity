325275d7eada970099e3b777422c03b388b7737a
Grace Wei <gracetw210@gmail.com>
2024-06-27 01:30:23 -0400

    Fix learning screen issues

--- green-invest/src/app/learn/learn.component.ts
+++ green-invest/src/app/learn/learn.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-learn',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './learn.component.html',
   styleUrl: './learn.component.css'
 })
 export class LearnComponent {
   title = 'green-invest';
   activeTab: string = 'Tab1';
+
   openTab(tabName: string): void {
     this.activeTab = tabName;
     console.log(this.activeTab);
