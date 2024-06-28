11ada66180be572d282615a0e9b2905a18bd777e
Vladimir <vg377@njit.edu>
2024-06-28 11:24:25 -0400

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,1 @@
-export const environment = {
-    production: true,
-    supabaseApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpZWxiZWNsamRxZm1iZWR1emJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxOTQ5MDk0MSwiZXhwIjoyMDM1MDY2OTQxfQ.5snIRxRxzy478LBvrDrl9lCP0lVHj_LgjwDy83FpLUs',
-    huggingfaceApiKey: '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'
-};
+
